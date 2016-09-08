0050e5f3f718915ae7047943964bf0df475e8667
benBen <benmbenjamin@gmail.com>
2016-09-08 12:58:51 -0700

    music structure

--- static/js/main.js
+++ static/js/main.js
@@ -1,1 +1,18 @@
-console.log('yup')
+$(document).ready(function(){
+
+  function poofHTML(target){
+    $.ajax({
+      url: target,
+      method: 'GET',
+      success: function(res){
+        $('body').append(res);
+        var state = target.split('/').pop();
+        window.history.pushState(null, null, state);
+      }
+    })
+  }
+
+  $('.nav-link').on('click', function(){
+    poofHTML($(this).data('link'));
+  })
+})
